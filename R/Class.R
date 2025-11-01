82fcc07f6356e238e30624f34d107aa20581548b
Sam El-Kamand <sam.elkamand@gmail.com>
2025-11-01 14:53:22 +1100

    fix: updated mol2 parsing approach

--- R/Class.R
+++ R/Class.R
@@ -1,4 +1,3 @@
-
 #' Create Structure Object
 #'
 #' @param atoms data.frame of atoms with columns:  eleno (element number) and elena (element name)
@@ -13,13 +12,11 @@ Structure <- S7::new_class(
     atoms = S7::class_data.frame,
     bonds = S7::class_data.frame
   ),
-
-  validator = function(self){
-
+  validator = function(self) {
     # Validate Bond Columns
     required_bond_cols <- c("from", "to")
     observed_bond_cols <- colnames(self@bonds)
-    if(!any(required_bond_cols %in% observed_bond_cols)){
+    if (!any(required_bond_cols %in% observed_bond_cols)) {
       missing <- setdiff(required_bond_cols, observed_bond_cols)
       toString("@bonds data.frame is missing required column/s: ", missing)
     }
@@ -27,28 +24,9 @@ Structure <- S7::new_class(
     # Validate Atom Columns
     required_atom_cols <- c("eleno", "elena", "x", "y", "z")
     observed_atom_cols <- colnames(self@atoms)
-    if(!any(required_atom_cols %in% observed_atom_cols)){
+    if (!any(required_atom_cols %in% observed_atom_cols)) {
       missing <- setdiff(required_atom_cols, observed_atom_cols)
       toString("@atoms data.frame is missing required column/s: ", missing)
     }
   }
 )
-
-
-
-#
-# Range <- S7::new_class("Range",
-#                    properties = list(
-#                      start = S7::class_numeric,
-#                      end = S7::class_numeric
-#                    ),
-#                    validator = function(self) {
-#                      if (length(self@start) != 1) {
-#                        "@start must be a single number"
-#                      } else if (length(self@end) != 1) {
-#                        "@end must be a single number"
-#                      } else if (self@end < self@start) {
-#                        "@end must be great than or equal to @start"
-#                      }
-#                    }
-# )
--- R/Parse.R
+++ R/Parse.R
@@ -9,47 +9,81 @@
 #' mol2 <- system.file(package="structures", "benzene.mol2")
 #' read_mol2(mol2)
 read_mol2 <- function(path){
-  lines <- readLines(path)
-  lines <- sub(x=lines, pattern = "^[[:space:]]+", replacement = "")
-  atom_marker <-"@<TRIPOS>ATOM"
-  atom_marker_index <- head(grep(pattern = atom_marker, x = lines, fixed = TRUE), n = 1)
-  if(length(atom_marker_index) != 1) stop("Failed to find Atom data record header (@<TRIPOS>ATOM). Are you sure your file is mol2?")
-
-  atom_start <- atom_marker_index + 1
-
-  lines_with_at <- grep(pattern = "^@", x = lines)
-  atom_end <- lines_with_at[lines_with_at>atom_marker_index][1]-1
-  if(length(atom_end) == 0) atom_end <- length(lines)
-
-  atom_lines <- lines[atom_start:atom_end]
-
-  ls_atoms  <- lapply(atom_lines, function(row){
-    elements <- unlist(strsplit(x = row, split = "[[:space:]]+"))
-    data.frame(
-      eleno = pluck(elements, 1),
-      elena = pluck(elements, 2),
-      x = pluck(elements, 3),
-      y = pluck(elements, 4),
-      z = pluck(elements, 5),
-      atom_type = pluck(elements, 6),
-      subst_id = pluck(elements, 7),
-      subst_name = pluck(elements, 8),
-      charge = pluck(elements, 9),
-      status_bit = pluck(elements, 10)
-    )
+  contents <- readr::read_file(path)
+  ls_contents <- unlist(strsplit(contents, split = "@"))
+  ls_contents <- Filter(function(v){ f = nchar(v) > 0}, x=ls_contents)
+  ls_contents_substplit <- lapply(ls_contents, function(x){
+    val <- unlist(strsplit(x, split =  "\n"));
+    val <- sub(x=val, pattern = "^[[:space:]]+", replacement = "")
+    return(val)
   })
 
-  df_atoms <- do.call(rbind, args = ls_atoms)
+  names(ls_contents_substplit) <- vapply(ls_contents_substplit, FUN = function(v){v[1]}, FUN.VALUE = character(1))
+  ls_contents_substplit_named <- lapply(ls_contents_substplit, FUN = function(v){tail(v, n=-2)})
 
-
-  return(df_atoms)
-
-  # bond_marker <-"@<TRIPOS>BOND"
-  # bond_marker_index <- head(grep(pattern = bond_marker, x = lines, fixed = TRUE), n = 1)
-  # bond_start <- bond_marker_index + 1
+  names(ls_contents_substplit_named) <- sub(x=names(ls_contents_substplit_named), pattern = "<TRIPOS>", replacement = "")
+  return(ls_contents_substplit_named)
 }
 
-pluck <- function(vec, i, outside = NA){
-  if(i < 0 | i > length(vec)) {return(outside)}
-  return(vec[i])
-}
+# read_mol2 <- function(path){
+#   lines <- readLines(path)
+#   lines <- sub(x=lines, pattern = "^[[:space:]]+", replacement = "")
+#   atom_marker <-"@<TRIPOS>ATOM"
+#   atom_marker_index <- head(grep(pattern = atom_marker, x = lines, fixed = TRUE), n = 1)
+#   if(length(atom_marker_index) != 1) stop("Failed to find Atom data record header (@<TRIPOS>ATOM). Are you sure your file is mol2?")
+#
+#   atom_start <- atom_marker_index + 1
+#
+#   lines_with_at <- grep(pattern = "^@", x = lines)
+#   atom_end <- lines_with_at[lines_with_at>atom_marker_index][1]-1
+#   if(length(atom_end) == 0) atom_end <- length(lines)
+#
+#   atom_lines <- lines[atom_start:atom_end]
+#
+#   ls_atoms  <- lapply(atom_lines, function(row){
+#     elements <- unlist(strsplit(x = row, split = "[[:space:]]+"))
+#     data.frame(
+#       eleno = pluck(elements, 1),
+#       elena = pluck(elements, 2),
+#       x = pluck(elements, 3),
+#       y = pluck(elements, 4),
+#       z = pluck(elements, 5),
+#       atom_type = pluck(elements, 6),
+#       subst_id = pluck(elements, 7),
+#       subst_name = pluck(elements, 8),
+#       charge = pluck(elements, 9),
+#       status_bit = pluck(elements, 10)
+#     )
+#   })
+#
+#   df_atoms <- do.call(rbind, args = ls_atoms)
+#
+#
+#   return(df_atoms)
+#
+#   # bond_marker <-"@<TRIPOS>BOND"
+#   # bond_marker_index <- head(grep(pattern = bond_marker, x = lines, fixed = TRUE), n = 1)
+#   # bond_start <- bond_marker_index + 1
+# }
+#
+# pluck <- function(vec, i, outside = NA){
+#   if(i < 0 | i > length(vec)) {return(outside)}
+#   return(vec[i])
+# }
+
+
+fileName <- 'inst/benzene.mol2'
+contents <- readr::read_file(fileName)
+ls_contents <- unlist(strsplit(contents, split = "@"))
+ls_contents <- Filter(function(v){ f = nchar(v) > 0}, x=ls_contents)
+ls_contents_substplit <- lapply(ls_contents, function(x){
+  val <- unlist(strsplit(x, split =  "\n"));
+  val <- sub(x=val, pattern = "^[[:space:]]+", replacement = "")
+  return(val)
+  })
+
+names(ls_contents_substplit) <- vapply(ls_contents_substplit, FUN = function(v){v[1]}, FUN.VALUE = character(1))
+ls_contents_substplit_named <- lapply(ls_contents_substplit, FUN = function(v){tail(v, n=-2)})
+
+names(ls_contents_substplit) <- sub(x=names(ls_contents_substplit_named), pattern = "<TRIPOS>", replacement = "")
+return(ls_contents_substplit)
